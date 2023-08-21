a3778eee151e07907bffcb7dadbd54d5365a3f65
sachinsonie <sachinkataria708@gmail.com>
2023-08-21 10:42:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pwjavalearn
+# pwjava
---
+++ basicfunction.java
@@ -0,0 +1,11 @@
+
+public class basicfunction {
+    public static void printbasic(){
+        for(int a=1; a<=10; a++){
+        System.out.println(a + ":" + "i am millionaire");}
+    }
+    public static void main(String[] args) {
+        printbasic(); //fuction call
+        
+    }
+}
